f1f126186d6dafd42b5a6b410b99013c2e8a0f6b
shiva <st172239@gmail.com>
2021-06-23 09:38:22 +0545

    Chats screen completed

---
+++ whatsapp_ui/lib/Models/ChatsModels.dart
@@ -0,0 +1,85 @@
+class ChatsModels {
+  final String name;
+  final String message;
+  final String time;
+  final String ProfileUrl;
+  ChatsModels(
+      {required this.ProfileUrl,
+      required this.message,
+      required this.name,
+      required this.time});
+}
+
+  List<ChatsModels> DummyData = [
+    ChatsModels(
+      ProfileUrl:
+          "https://avatars.githubusercontent.com/u/39731617?s=400&u=5e5272d15b575879ad02a57d378d8a03360bfc53&v=4",
+      message: "Welcome",
+      name: "Shiva tiwari",
+      time: "2:30 PM",
+    ),
+     ChatsModels(
+      ProfileUrl:
+          "https://avatars.githubusercontent.com/u/39731617?s=400&u=5e5272d15b575879ad02a57d378d8a03360bfc53&v=4",
+      message: "Welcome",
+      name: "Shiva tiwari",
+      time: "2:30 PM",
+    ),
+     ChatsModels(
+      ProfileUrl:
+          "https://avatars.githubusercontent.com/u/39731617?s=400&u=5e5272d15b575879ad02a57d378d8a03360bfc53&v=4",
+      message: "Welcome",
+      name: "Shiva tiwari",
+      time: "2:30 PM",
+    ),
+     ChatsModels(
+      ProfileUrl:
+          "https://avatars.githubusercontent.com/u/39731617?s=400&u=5e5272d15b575879ad02a57d378d8a03360bfc53&v=4",
+      message: "Welcome",
+      name: "Shiva tiwari",
+      time: "2:30 PM",
+    ),
+         ChatsModels(
+      ProfileUrl:
+          "https://avatars.githubusercontent.com/u/39731617?s=400&u=5e5272d15b575879ad02a57d378d8a03360bfc53&v=4",
+      message: "Welcome",
+      name: "Shiva tiwari",
+      time: "2:30 PM",
+    ),
+     ChatsModels(
+      ProfileUrl:
+          "https://avatars.githubusercontent.com/u/39731617?s=400&u=5e5272d15b575879ad02a57d378d8a03360bfc53&v=4",
+      message: "Welcome",
+      name: "Shiva tiwari",
+      time: "2:30 PM",
+    ),
+     ChatsModels(
+      ProfileUrl:
+          "https://avatars.githubusercontent.com/u/39731617?s=400&u=5e5272d15b575879ad02a57d378d8a03360bfc53&v=4",
+      message: "Welcome",
+      name: "Shiva tiwari",
+      time: "2:30 PM",
+    ),
+         ChatsModels(
+      ProfileUrl:
+          "https://avatars.githubusercontent.com/u/39731617?s=400&u=5e5272d15b575879ad02a57d378d8a03360bfc53&v=4",
+      message: "Welcome",
+      name: "Shiva tiwari",
+      time: "2:30 PM",
+    ),
+     ChatsModels(
+      ProfileUrl:
+          "https://avatars.githubusercontent.com/u/39731617?s=400&u=5e5272d15b575879ad02a57d378d8a03360bfc53&v=4",
+      message: "Welcome",
+      name: "Shiva tiwari",
+      time: "2:30 PM",
+    ),
+     ChatsModels(
+      ProfileUrl:
+          "https://avatars.githubusercontent.com/u/39731617?s=400&u=5e5272d15b575879ad02a57d378d8a03360bfc53&v=4",
+      message: "Welcome",
+      name: "Shiva tiwari",
+      time: "2:30 PM",
+    )
+  ];
+    
--- whatsapp_ui/lib/pages/ChatsPage.dart
+++ whatsapp_ui/lib/pages/ChatsPage.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/Models/ChatsModels.dart';
 class ChatsScreen extends StatelessWidget {
   const ChatsScreen({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Chats Page"),
-      
+    return ListView.builder(
+      itemCount: DummyData.length ,
+      itemBuilder: (context, index){
+        return Column(
+          children: [
+            InkWell(
+              onTap: (){
+                print("object");
+              },
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Colors.grey,
+                  backgroundImage: NetworkImage(DummyData[index].ProfileUrl,),
+                ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(DummyData[index].name, style: TextStyle(fontSize: 20,),),
+                    Text(DummyData[index].time)
+                  ],
+                ),
+                subtitle: Text(DummyData[index].message, style: TextStyle(fontSize: 16, fontWeight:FontWeight.bold,),)
+              ),
+            )
+          ]
+        );
+      },
     );
   }
 }
